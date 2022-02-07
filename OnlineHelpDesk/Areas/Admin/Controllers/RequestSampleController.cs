4e934229bb8b2e29d805a8839e79d7580bb15d9c
Thien Nguyen <j.thien1551@gmail.com>
2022-02-07 14:44:41 +0700

    Override Identity Pages

--- OnlineHelpDesk/Areas/Admin/Controllers/RequestSampleController.cs
+++ OnlineHelpDesk/Areas/Admin/Controllers/RequestSampleController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,28 @@ namespace OnlineHelpDesk.Areas.Admin.Controllers
 {
     public class RequestSampleController : Controller
     {
+        private readonly Data.ApplicationDbContext _context;
+        public RequestSampleController(Data.ApplicationDbContext _context) 
+        {
+            this._context = _context;
+        
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult RequestSampleList()
+        {
+            var requestList = _context.RequestSample.ToList();
+            return View(requestList);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.data = new SelectList(_context.FacilityCategory.ToList(), "FacilityCategoryId", "CategoryName");
+            return View();
+        }
+
     }
 }
