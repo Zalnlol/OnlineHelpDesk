3d83d1b9722eb5561532a92f7ee3a45b495f92c5
Lê Nguyễn Thành Nhân <74607205+lenhannhan123@users.noreply.github.com>
2022-02-09 12:40:07 +0700

    update dashboard

---
+++ OnlineHelpDesk/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineHelpDesk.Data;
+using OnlineHelpDesk.Models;
+using OnlineHelpDesk.Areas.Admin.Models;
+
+
+
+
+namespace OnlineHelpDesk.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> _userManager)
+        {
+            this.db = db;
+            this._userManager = _userManager;
+        
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+        
+
+        [HttpPost]
+        public IActionResult CreateAccount(RegisterUserModel registerUserModel)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public bool checkemail(string mail)
+        {
+
+            var email = db.Users.SingleOrDefault(t => t.Email.ToLower().Equals(mail.Trim().ToLower())).Email;
+
+            if (email !=null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
