b7e48c1cd2d2e9f8c24b5ab977020f1eb203b223
vuongpham99 <86349987+vuongpham99@users.noreply.github.com>
2022-02-15 22:42:44 +0700

    update request

---
+++ OnlineHelpDesk/Models/Request.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineHelpDesk.Models
+{
+    public class Request
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RequestId { get; set; }
+
+        public String RequestorId { get; set; }
+
+        [Required(ErrorMessage = "Facility is required!")]
+        public String FacilityId { get; set; }
+
+        [BindProperty, DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime RequestTime { get; set; }
+
+        public String Status { get; set; }
+
+        [BindProperty, DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Start date is required!")]
+        public DateTime StartDate { get; set; }
+
+        [BindProperty, DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "End date is required!")]
+        public DateTime EndDate { get; set; }
+
+        public String Remark { get; set; }
+    }
+}
